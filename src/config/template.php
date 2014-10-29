d844ab947a54b54e458441777fda1fdfeecdf470
nurmanhabib <nurmanhabib@yahoo.co.id>
2014-10-29 22:47:19 +0700

    add new template: treeview

--- src/config/template.php
+++ src/config/template.php
@@ -6,5 +6,7 @@ return array(
     'pills'             => 'Nurmanhabib\\Navigator\\Template\\Pills',
     'sb-admin-2'        => 'Nurmanhabib\\Navigator\\Template\\SBAdmin2',
     'sb-admin-2-child'  => 'Nurmanhabib\\Navigator\\Template\\SBAdmin2\\Child',
+    'treeview'          => 'Nurmanhabib\\Navigator\\Template\\Treeview',
+    'treeview-child'    => 'Nurmanhabib\\Navigator\\Template\\Treeview\\Child',
 
 );
---
+++ src/views/treeview-child.blade.php
@@ -0,0 +1,5 @@
+<ul class="treeview-menu">
+    
+    {{ $list }}
+
+</ul>
